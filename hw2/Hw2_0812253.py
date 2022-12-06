80c450d1d73dd21671333b9661f3b04f1dfc91bf
DenChenn <aaaa102234@gmail.com>
2022-12-06 13:49:11 +0800

    feat: build model and enable it to run

--- hw2/Hw2_0812253.py
+++ hw2/Hw2_0812253.py
@@ -3,6 +3,9 @@ import torch
 import pandas as pd
 from sklearn.model_selection import train_test_split
 import torch.nn as nn
+import torch.optim as optim
+import time
+
 
 P_TRAIN_CSV = 'p_train.csv'
 P_VALID_CSV = 'p_val.csv'
@@ -17,6 +20,8 @@ class Preprocess:
         self.test_iter = None
         self.val_iter = None
         self.train_iter = None
+        self.input_dim = None
+        self.embedding_dim = None
 
     def select_and_save(self):
         self.train_df = self.train_df[['Utterance', 'Emotion']]
@@ -50,13 +55,19 @@ class Preprocess:
         # the model will use the GloVe word vectors trained on:
         # 6 billion words with 100 dimensions per word
         text.build_vocab(train, vectors="glove.6B.100d")
+        label.build_vocab(train)
+        self.embedding_dim = 100
+        self.input_dim = len(text.vocab)
+        print(text.vocab.vectors.shape)
+        print(f"Unique tokens in TEXT vocabulary: {len(text.vocab)}")
+        print(f"Unique tokens in LABEL vocabulary: {len(label.vocab)}")
 
         # this will return the iterator for each dataset
         # in each dataset, it is sorted by the length of text
         # the device argument is used to specify using the CPU
         self.train_iter, self.val_iter, self.test_iter = Iterator.splits(
             (train, val, test),
-            sort_key=lambda x: len(x.Text),
+            sort_key=lambda x: len(x.Utterance),
             batch_sizes=(32, 256, 256),
             device=-1
         )
@@ -86,6 +97,99 @@ class RNN(nn.Module):
         return self.fc(hidden.squeeze(0))
 
 
+def binary_accuracy(preds, y):
+    """
+    Returns accuracy per batch, i.e. if you get 8/10 right, this returns 0.8, NOT 8
+    """
+
+    # round predictions to the closest integer
+    rounded_preds = torch.round(torch.sigmoid(preds))
+    # convert into float for division
+    correct = (rounded_preds == y).float()
+    acc = correct.sum() / len(correct)
+    return acc
+
+
+def train(model, iterator, optimizer, criterion):
+    epoch_loss = 0
+    epoch_acc = 0
+    model.train()
+
+    for batch in iterator:
+        optimizer.zero_grad()
+        predictions = model(batch.Utterance).squeeze(1)
+        loss = criterion(predictions, batch.Emotion)
+        acc = binary_accuracy(predictions, batch.Emotion)
+        loss.backward()
+
+        optimizer.step()
+
+        epoch_loss += loss.item()
+        epoch_acc += acc.item()
+
+    return epoch_loss / len(iterator), epoch_acc / len(iterator)
+
+
+def evaluate(model, iterator, criterion):
+    epoch_loss = 0
+    epoch_acc = 0
+    model.eval()
+
+    with torch.no_grad():
+        for batch in iterator:
+            predictions = model(batch.Utterance).squeeze(1)
+            loss = criterion(predictions, batch.Emotion)
+            acc = binary_accuracy(predictions, batch.Emotion)
+
+            epoch_loss += loss.item()
+            epoch_acc += acc.item()
+
+    return epoch_loss / len(iterator), epoch_acc / len(iterator)
+
+
+def epoch_time(start_time, end_time):
+    elapsed_time = end_time - start_time
+    elapsed_mins = int(elapsed_time / 60)
+    elapsed_secs = int(elapsed_time - (elapsed_mins * 60))
+    return elapsed_mins, elapsed_secs
+
+
 if __name__ == '__main__':
     pre = Preprocess('train.csv', 'dev.csv')
     pre.run()
+
+    hidden_dim = 256
+    output_dim = 1
+    model = RNN(pre.input_dim, pre.embedding_dim, hidden_dim, output_dim)
+
+    optimizer = optim.SGD(model.parameters(), lr=1e-3)
+    criterion = nn.BCEWithLogitsLoss()
+
+    N_EPOCHS = 20
+    best_valid_loss = float('inf')
+
+    for epoch in range(N_EPOCHS):
+
+        start_time = time.time()
+
+        train_loss, train_acc = train(model, pre.train_iter, optimizer, criterion)
+        valid_loss, valid_acc = evaluate(model, pre.val_iter, criterion)
+
+        end_time = time.time()
+
+        epoch_mins, epoch_secs = epoch_time(start_time, end_time)
+
+        if valid_loss < best_valid_loss:
+            best_valid_loss = valid_loss
+            torch.save(model.state_dict(), 'tut1-model.pt')
+
+        print(f'Epoch: {epoch+1:02} | Epoch Time: {epoch_mins}m {epoch_secs}s')
+        print(f'\tTrain Loss: {train_loss:.3f} | Train Acc: {train_acc*100:.2f}%')
+        print(f'\t Val. Loss: {valid_loss:.3f} |  Val. Acc: {valid_acc*100:.2f}%')
+
+    model.load_state_dict(torch.load('tut1-model.pt'))
+    test_loss, test_acc = evaluate(model, pre.test_iter, criterion)
+
+    print(f'Test Loss: {test_loss:.3f} | Test Acc: {test_acc*100:.2f}%')
+
+
